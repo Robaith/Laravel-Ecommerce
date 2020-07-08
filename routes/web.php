8eaa9798102523f2dccd464223e0d7a8afdbed13
Robaith <robaith@gmail.com>
2020-07-08 15:38:25 +0600

    frontend index theme primary customized

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 
 
-Route::get('/', function () {return view('welcome');});
+Route::get('/', function () {return view('pages.index');});
 //auth & user
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
